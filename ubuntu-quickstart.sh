543b4670041cfacf73488877bcb42af67402b49d
Olivier Grisel <olivier.grisel@ensta.org>
2013-08-29 13:29:35 +0200

    Make the script work under azure using more variables

--- ubuntu-quickstart.sh
+++ ubuntu-quickstart.sh
@@ -11,7 +11,7 @@ sudo apt-get install -y \
 
 sudo update-alternatives --set editor /usr/bin/vim.basic
 
-cd ~
+cd $HOME
 if [ ! -d "venv" ]; then
     virtualenv --system-site-packages venv
 fi
@@ -22,25 +22,36 @@ pip install scikit-learn ipython[notebook]
 git config --global user.name "Olivier Grisel"
 git config --global user.email olivier.grisel@ensta.org
 
-if [ ! -x "~/.ssh/config" ]; then
-    echo "Host github.com" >> ~/.ssh/config
-    echo "    StrictHostKeyChecking no" >> ~/.ssh/config
+if [ ! -x "$HOME/.ssh/config" ]; then
+    if f [ ! -d "$HOME/.ssh" ];
+        mkdir $HOME//.ssh
+    fi
+    echo "Host github.com" >> $HOME/.ssh/config
+    echo "    StrictHostKeyChecking no" >> $HOME/.ssh/config
 fi
 
-if [ ! -d "notebooks" ]; then
+if [ ! -d "$HOME/notebooks" ]; then
     git clone git@github.com:ogrisel/notebooks.git
 fi
 
-if [ ! -d "/mnt/ubuntu" ]; then
-    sudo mkdir /mnt/ubuntu
-    sudo chown -R ubuntu. /mnt/ubuntu
+if [ -d "/mnt/resource" ]; then
+    # Azure
+    DATA_ROOT = /mnt/resource
+else
+    # EC2
+    DATA_ROOT = /mnt
+fi
 
-    mkdir /mnt/ubuntu/data
-    ln -s /mnt/ubuntu/data
+if [ ! -d "$DATA_ROOT/$USER" ]; then
+    sudo mkdir $DATA_ROOT/$USER
+    sudo chown -R $USER. $DATA_ROOT/$USER
+
+    mkdir $DATA_ROOT/$USER/data
+    ln -s $DATA_ROOT/$USER/data $HOME/data
 fi
 
 # (Re)start the notebook process
-cd ~/notebooks
+cd $HOME/notebooks
 pkill -9 -f "disabled-ipython-browser" || echo "Nothing to kill"
 nohup ~/venv/bin/ipython notebook \
     --ip="*" \
